8ba47c4f8c11e879e35ec717c05565bf04ac177e
Davosky555 <davidilogho@gmail.com>
2022-08-07 05:42:29 -0400

    Update mwwl8422.py

--- mwwl8422.py
+++ mwwl8422.py
@@ -311,50 +311,50 @@ def sl3_datetime():
     return sl3_date, sl3_time
 
 
-def ports_tag_message_append(flag, val, f, typ):
-
+def ports_tag_message_append(flag, val, typ):
+    msg = ""
     if typ == 1:
-        f.write(flag)
+        msg += flag
         if -99999.0 not in val[:3]:
-            f.write("{0}{1}{2}{3}{4}\r\n".format("{:>11.3f}".format(val[0]), "{:>9.3f}".format(val[1]),
-                                                 "{:>10.0f}".format(val[2]), "{:>10.1f}".format(val[3]),
-                                                 "{:>10.1f}".format(val[4])))
+            msg += "{0}{1}{2}{3}{4}\r\n".format("{:>11.3f}".format(val[0]), "{:>9.3f}".format(val[1]),
+                                                "{:>10.0f}".format(val[2]), "{:>10.1f}".format(val[3]),
+                                                "{:>10.1f}".format(val[4]))
         else:
-            f.write("  Data flagged as bad or missing\r\n")
+            msg += "  Data flagged as bad or missing\r\n"
     elif typ == 2:
-        f.write(flag)
+        msg += flag
         if -99999.0 not in val:
-            f.write("{0}{1}{2}\r\n".format("{:>11.1f}".format(val[0]),
-                                           "{:>9.0f}".format(val[1]),
-                                           "{:>10.1f}".format(val[2])))
+            msg += "{0}{1}{2}\r\n".format("{:>11.1f}".format(val[0]),
+                                          "{:>9.0f}".format(val[1]),
+                                          "{:>10.1f}".format(val[2]))
         else:
-            f.write("  Data flagged as bad or missing\r\n")
+            msg += "  Data flagged as bad or missing\r\n"
 
     elif typ == 3:
-        f.write(flag)
+        msg += flag
         if -99999.0 not in val:
-            f.write("{0}{1}{2}\r\n".format("{:>11.3f}".format(val[0]),
-                                           "{:>9.3f}".format(val[1]),
-                                           "{:>10.0f}".format(val[2])))
+            msg += "{0}{1}{2}\r\n".format("{:>11.3f}".format(val[0]),
+                                          "{:>9.3f}".format(val[1]),
+                                          "{:>10.0f}".format(val[2]))
         else:
-            f.write("  Data flagged as bad or missing\r\n")
+            msg += "  Data flagged as bad or missing\r\n"
     elif typ == 4:
-        f.write(flag)
-        f.write("{:>11.1f}\r\n".format(val) if val != -99999.0 else "  Data Flagged as bad or missing\r\n")
+        msg += flag
+        msg += "{:>11.1f}\r\n".format(val) if val != -99999.0 else "  Data Flagged as bad or missing\r\n"
 
     elif typ == 5:
-        f.write(flag)
-        f.write("{:>10.2f}\r\n".format(val) if val != -99999.0 else " Data Flagged as bad or missing\r\n")
+        msg += flag
+        msg += "{:>10.2f}\r\n".format(val) if val != -99999.0 else " Data Flagged as bad or missing\r\n"
 
     elif typ == 6:
-        f.write(flag)
-        f.write("{:>10.3f}".format(val) + "\r\n" if val != -99999.0 else " data not available\r\n")
+        msg += flag
+        msg += "{:>10.3f}".format(val) + "\r\n" if val != -99999.0 else " data not available\r\n"
 
     elif typ == 7:
         for v in val:
-            f.write(flag)
-            f.write("{:>11.3f}".format(v) + "\r\n" if v != -99999.0 else " data not available\r\n")
-    return
+            msg += flag
+            msg += "{:>11.3f}".format(v) + "\r\n" if v != -99999.0 else " data not available\r\n"
+    return msg
 
 
 def ports_tag_message_formatter():
@@ -389,12 +389,11 @@ def ports_tag_message_formatter():
     time_tag_goes = "0" + add_sns[0].get_encoded_sutron_day() + add_sns[0].get_encoded_hour()
     min_goes = add_sns[0].get_encoded_minute()
     sys_goes = "@@"
-    f = open("p", "w")
-    f.write("NOS {0} {1} {2}:{3}:{4}\r\n".format(station_id, pri_date, pri_hour, pri_minute, pri_second))
+    ports_tag_msg = "NOS {0} {1} {2}:{3}:{4}\r\n".format(station_id, pri_date, pri_hour, pri_minute, pri_second)
     for a_s in add_sns:
         if a_s.label in ("AQT", "AQTSTD", "AQTOUT", "AQT1", "AQT2"):
             if a_s.label == "AQT":
-                if a_s.meas_number == 1:
+                if a_s.meas_number == str("M1"):
                     pri_sns_check = a_s.label
                 aqt_temp = a_s.get_encoded_data()
                 aqt_rtemp = a_s.get_encoded_redundant_data()
@@ -412,10 +411,10 @@ def ports_tag_message_formatter():
                     pri_sns += "1" + aqt_temp + aqt_std_temp + aqt_out_temp + aqt1_temp + aqt2_temp + ">" + aqt_rtemp
                 else:
                     aqt_goes += "1" + aqt_temp + aqt_std_temp + aqt_out_temp + aqt1_temp + aqt2_temp + ">" + aqt_rtemp
-                ports_tag_message_append("A1 1", aqt, f, 1)
+                ports_tag_msg += ports_tag_message_append("A1 1", aqt, 1)
         elif a_s.label in ("WS", "WD", "WG"):
             if a_s.label == "WS":
-                if a_s.meas_number == 1:
+                if a_s.meas_number == str("M1"):
                     pri_sns_check = a_s.label
                 ws_temp = a_s.get_encoded_data()
             if a_s.label == "WD":
@@ -428,10 +427,10 @@ def ports_tag_message_formatter():
                     pri_sns += "3" + ws_temp + wd_temp + wg_temp
                 else:
                     wind_goes += "3" + ws_temp + wd_temp + wg_temp
-                ports_tag_message_append("C1 3", wind1, f, 2)
+                ports_tag_msg += ports_tag_message_append("C1 3", wind1, 2)
         elif a_s.label in ("WS2", "WD2", "WG2"):
             if a_s.label == "WS2":
-                if a_s.meas_number == 1:
+                if a_s.meas_number == str("M1"):
                     pri_sns_check = a_s.label
                 ws_temp2 = a_s.get_encoded_data()
             if a_s.label == "WD2":
@@ -444,11 +443,11 @@ def ports_tag_message_formatter():
                     pri_sns += "3" + ws_temp2 + wd_temp2 + wg_temp2
                 else:
                     wind_goes2 += "3" + ws_temp2 + wd_temp2 + wg_temp2
-                ports_tag_message_append("C1 3", wind1, f, 2)
+                ports_tag_msg += ports_tag_message_append("C1 3", wind1, 2)
 
         elif a_s.label in ("MWWL", "MWSTD", "MWOUT"):
             if a_s.label == "MWWL":
-                if a_s.meas_number == 1:
+                if a_s.meas_number == str("M1"):
                     pri_sns_check = a_s.label
                 mw_temp = a_s.get_encoded_data()
                 mw_rtemp = a_s.get_encoded_redundant_data()
@@ -458,17 +457,16 @@ def ports_tag_message_formatter():
                 mw_out_temp = a_s.get_encoded_data()
             mwwl1.append(a_s.value)
             if len(mwwl1) == 3:
-                print("mw_out_temp", mw_out_temp)
                 if pri_sns_check == "MWWL":
                     pri_sns += "8" + mw_temp + mw_std_temp + mw_out_temp + "#" + mw_rtemp
                 else:
 
                     mw_goes += "8" + mw_temp + mw_std_temp + mw_out_temp + "#" + mw_rtemp
-                ports_tag_message_append("Y1 8", mwwl1, f, 3)
+                ports_tag_msg += ports_tag_message_append("Y1 8", mwwl1, 3)
 
         elif a_s.label in ("MWWL2", "MWSTD2", "MWOUT2"):
             if a_s.label == "MWWL2":
-                if a_s.meas_number == 1:
+                if a_s.meas_number == str("M1"):
                     pri_sns_check = a_s.label
                 mw_temp2 = a_s.get_encoded_data()
                 mw_rtemp2 = a_s.get_encoded_redundant_data()
@@ -482,11 +480,11 @@ def ports_tag_message_formatter():
                     pri_sns += "8" + mw_temp2 + mw_std_temp2 + mw_out_temp2 + "#" + mw_rtemp2
                 else:
                     mw_goes2 += "8" + mw_temp2 + mw_std_temp2 + mw_out_temp2 + "#" + mw_rtemp2
-                ports_tag_message_append("Y1 8", mwwl2, f, 3)
+                ports_tag_msg += ports_tag_message_append("Y2 8", mwwl2, 3)
 
         elif a_s.label in ("BWL", "BWLSTD", "BWLOUT"):
             if a_s.label == "BWL":
-                if a_s.meas_number == 1:
+                if a_s.meas_number == str("M1"):
                     pri_sns_check = a_s.label
                 bwl_temp = a_s.get_encoded_data()
                 bwl_rtemp = a_s.get_encoded_redundant_data()
@@ -500,63 +498,64 @@ def ports_tag_message_formatter():
                     pri_sns += "2" + bwl_temp + bwl_std_temp + bwl_out_temp + '"' + bwl_rtemp
                 else:
                     bwl_goes += "2" + bwl_temp + bwl_std_temp + bwl_out_temp + '"' + bwl_rtemp
-                ports_tag_message_append("Y1 8", bwl, f, 3)
+                ports_tag_msg += ports_tag_message_append("Y1 8", bwl, 3)
         elif a_s.label == "AT":
-            if a_s.meas_number == 1:
+            if a_s.meas_number == str("M1"):
                 pri_sns += "4" + a_s.get_encoded_data()
             else:
                 at_goes += "4" + a_s.get_encoded_data()
-            ports_tag_message_append("D1 4", a_s.value, f, 4)
+            ports_tag_msg += ports_tag_message_append("D1 4", a_s.value, 4)
         elif a_s.label == "WT":
-            if a_s.meas_number == 1:
+            if a_s.meas_number == str("M1"):
                 pri_sns += "5" + a_s.get_encoded_data()
             else:
                 wt_goes += "5" + a_s.get_encoded_data()
-            ports_tag_message_append("E1 5", a_s.value, f, 4)
+            ports_tag_msg += ports_tag_message_append("E1 5", a_s.value, 4)
         elif a_s.label == "CTWT":
-            if a_s.meas_number == 1:
+            if a_s.meas_number == str("M1"):
                 pri_sns += "5" + a_s.get_encoded_data()
             else:
                 ctwt_goes += "5" + a_s.get_encoded_data()
-            ports_tag_message_append("E2 5", a_s.value, f, 4)
+            ports_tag_msg += ports_tag_message_append("E2 5", a_s.value, 4)
         elif a_s.label == "BARO":
-            if a_s.meas_number == 1:
+            if a_s.meas_number == str("M1"):
                 pri_sns += "6" + a_s.get_encoded_data()
             else:
                 baro_goes += "6" + a_s.get_encoded_data()
-            ports_tag_message_append("F1 6", a_s.value, f, 4)
+            ports_tag_msg += ports_tag_message_append("F1 6", a_s.value, 4)
         elif a_s.label == "BAT":
             bat_goes += "<" + a_s.get_encoded_data() + " "
-            ports_tag_message_append("L1 <", a_s.value, f, 4)
+            ports_tag_msg += ports_tag_message_append("L1 <", a_s.value, 4)
         elif a_s.label == "BATT":
             batt_goes += "<" + a_s.get_encoded_data() + " "
-            ports_tag_message_append("L1 <", a_s.value, f, 4)
+            ports_tag_msg += ports_tag_message_append("L1 <", a_s.value, 4)
         elif a_s.label == "COND":
-            if a_s.meas_number == 1:
+            if a_s.meas_number == str("M1"):
                 pri_sns += "-7" + a_s.get_encoded_data()
             else:
                 cond_goes += "-7" + a_s.get_encoded_data()
-            ports_tag_message_append("G1 -7", a_s.value, f, 5)
+            ports_tag_msg += ports_tag_message_append("G1 -7", a_s.value, 5)
         elif a_s.label in "SNS":
             dat_sns_goes = dat_sns_goes + a_s.get_encoded_data()
-            ports_tag_message_append(a_s.label, a_s.value, f, 6)
+            ports_tag_msg += ports_tag_message_append(a_s.label, a_s.value, 6)
         elif a_s.label == "DAT":
-            ports_tag_message_append(a_s.label, a_s.value, f, 6)
+            ports_tag_msg += ports_tag_message_append(a_s.label, a_s.value, 6)
             dat_sns_goes = a_s.get_encoded_data() + dat_sns_goes
-        elif a_s.label == "TWL":
+        elif a_s.label in ("AQTWL", "MWTWL"):
             a_s.get_encoded_tsunami()
             tsu = a_s.get_encoded_tsunami()
-            tsu_goes += "T" + tsu[0] + tsu[1] + tsu[2] + tsu[3] + tsu[4] + tsu[5] + tsu[6] + tsu[7] + tsu[8]
-            val = a_s.value, a_s.value2, a_s.value3, a_s.value4, a_s.value5, a_s.value6
-            ports_tag_message_append("U1", val, f, 7)
-    print("pri_sns", pri_sns)
+            if pri_sns_check[:2] == a_s.label[:2]:
+                tsu_goes += "T" + tsu[0] + tsu[1] + tsu[2] + tsu[3] + tsu[4] + tsu[5] + tsu[6] + tsu[7] + tsu[8]
+                val = a_s.value, a_s.value2, a_s.value3, a_s.value4, a_s.value5, a_s.value6
+                ports_tag_msg += ports_tag_message_append("U1", val, 7)
     goes_msg = "P{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}{11}{12}{13}{14}{15}{16}{17}{18}{19}".\
         format(station_id, dat_sns_goes, sys_goes, min_goes, time_tag_goes, pri_sns, aqt_goes, mw_goes,
                mw_goes2, wind_goes, wind_goes2, at_goes, wt_goes, ctwt_goes, baro_goes, cond_goes,
                bat_goes, bwl_goes, batt_goes, tsu_goes)
 
-    f.write("\r\nREPORT COMPLETE\r\n")
-    f.close()
+    ports_tag_msg += "\r\nREPORT COMPLETE\r\n"
+    with open("p", "w") as f:
+        f.write(ports_tag_msg)
 
 
 message_ready = True
@@ -611,11 +610,12 @@ cnt_meas = 0
 goes_msg = ""
 for s_n in range(32):
     if command_line("!M" + str(s_n + 1) + " active\r").strip() == "On":
-        cnt_meas += 1
+        if command_line("!M" + str(s_n + 1) + " LABEL\r").strip() not in ("MWCOUNTS", "MWCOUNTS2", "AQTCOUNTS"):
+            cnt_meas += 1
         if cnt_meas == 1 or command_line("!M" + str(s_n + 1) + " LABEL\r").strip() in ("AQT", "BWL", "MWWL", "MWWL2"):
             temp_sns.append(PrimarySensor("M" + str(s_n + 1)))
         else:
-            if command_line("!M" + str(s_n + 1) + " LABEL\r").strip() == "TWL":
+            if command_line("!M" + str(s_n + 1) + " LABEL\r").strip() in ("AQTWL", "MWTWL"):
                 temp_sns.append(TsunamiData("M" + str(s_n + 1)))
             else:
                 temp_sns.append(SecondarySensor("M" + str(s_n + 1)))
@@ -664,8 +664,10 @@ if "SNS" in temp_label:
     temp_label, temp_sns, add_sns = sort_sns_list(["SNS"], temp_label, temp_sns, add_sns)
 if "DAT" in temp_label:
     temp_label, temp_sns, add_sns = sort_sns_list(["DAT"], temp_label, temp_sns, add_sns)
-if "TWL" in temp_label:
-    temp_label, temp_sns, add_sns = sort_sns_list(["TWL"], temp_label, temp_sns, add_sns)
+if "AQTWL" in temp_label:
+    temp_label, temp_sns, add_sns = sort_sns_list(["AQTWL"], temp_label, temp_sns, add_sns)
+if "MWTWL" in temp_label:
+    temp_label, temp_sns, add_sns = sort_sns_list(["MWTWL"], temp_label, temp_sns, add_sns)
 del temp_sns
 del temp_label
 ports_tag_message_formatter()
@@ -684,11 +686,13 @@ def initialize_config():
 
     for i in range(32):
         if command_line("!M" + str(i + 1) + " active\r").strip() == "On":
-            cnt_meas += 1
-            if cnt_meas == 1:
+            if command_line("!M" + str(i + 1) + " LABEL\r").strip() not in ("MWCOUNTS", "MWCOUNTS2", "AQTCOUNTS"):
+                cnt_meas += 1
+            if cnt_meas == 1 or command_line("!M" + str(i + 1) + " LABEL\r").strip() in \
+                    ("AQT", "BWL", "MWWL", "MWWL2"):
                 temp_sns.append(PrimarySensor("M" + str(i + 1)))
             else:
-                if command_line("!M" + str(i + 1) + " LABEL\r").strip() == "TWL":
+                if command_line("!M" + str(i + 1) + " LABEL\r").strip() in ("AQTWL", "MWTWL"):
                     temp_sns.append(TsunamiData("M" + str(i + 1)))
                 else:
                     temp_sns.append(SecondarySensor("M" + str(i + 1)))
@@ -699,7 +703,7 @@ def initialize_config():
         temp_label, temp_sns, add_sns = sort_sns_list(("AQT", "AQTSTD", "AQTOUT", "AQT1", "AQT2"), temp_label, temp_sns,
                                                       add_sns)
     elif temp_label[0] == "MWWL":
-        temp_label, temp_sns, add_sns = sort_sns_list(("MWWL", "MWSTD", "MWOUT", "MWCOUNTS"), temp_label, temp_sns, add_sns)
+        temp_label, temp_sns, add_sns = sort_sns_list(("MWWL", "MWSTD", "MWOUT"), temp_label, temp_sns, add_sns)
 
     elif temp_label[0] == "WS":
         temp_label, temp_sns, add_sns = sort_sns_list(("WS", "WD", "WG"), temp_label, temp_sns, add_sns)
@@ -737,8 +741,10 @@ def initialize_config():
         temp_label, temp_sns, add_sns = sort_sns_list(["SNS"], temp_label, temp_sns, add_sns)
     if "DAT" in temp_label:
         temp_label, temp_sns, add_sns = sort_sns_list(["DAT"], temp_label, temp_sns, add_sns)
-    if "TWL" in temp_label:
-        temp_label, temp_sns, add_sns = sort_sns_list(["TWL"], temp_label, temp_sns, add_sns)
+    if "AQTWL" in temp_label:
+        temp_label, temp_sns, add_sns = sort_sns_list(["AQTWL"], temp_label, temp_sns, add_sns)
+    if "MWTWL" in temp_label:
+        temp_label, temp_sns, add_sns = sort_sns_list(["MWTWL"], temp_label, temp_sns, add_sns)
     del temp_sns
     del temp_label
     ports_tag_message_formatter()
@@ -824,13 +830,15 @@ def update_data():
     This task updates the sensor objects
     """
     status_message("Updating all data...")
-    print(add_sns)
-    print(cnt_meas)
+    for i in add_sns:
+        print(i.label, i.meas_number)
+    print(len(add_sns), "add_sns")
+    print(cnt_meas, "cnt_meas")
     for i in range(cnt_meas):
         if i == 0:
             add_sns[i].update_primary_data()
         else:
-            if add_sns[i].label == "TWL":
+            if add_sns[i].label in ("AQTWL", "MWTWL"):
                 add_sns[i].update_tsunami_data()
             else:
                 add_sns[i].update_secondary_data()
